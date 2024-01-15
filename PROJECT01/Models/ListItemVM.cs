e507be2b269c538e254a601e9040bbf0c4e307fc
Kazi Saharia <114473179+biplob65712@users.noreply.github.com>
2024-01-15 13:53:33 +0600

    Update ListItemVM.cs

--- PROJECT01/Models/ListItemVM.cs
+++ PROJECT01/Models/ListItemVM.cs
@@ -10,16 +10,4 @@ namespace PROJECT01.Models
         public string? Status { get; set; }
         public string? CategoryName { get; set; } 
     }
-
-
-
-    //public int Id { get; set; }
-
-    //public string Name { get; set; }
-
-    //public string? Description { get; set; }
-
-    //public double SalesPrice { get; set; }
-
-    //public string? BrandName { get; set; }
 }
